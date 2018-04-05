ba9abeae9f843504b5b3d41c75c29a0a3bc67f74
toddbirchard <toddbirchard@gmail.com>
2018-04-05 12:09:22 -0400

    refactor

--- src/js/base.js
+++ src/js/base.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function(){
-
+    var txt = $('.content')[0].textContent,
+    wordCount = txt.replace( /[^\w ]/g, "" ).split( /\s+/ ).length;
+    var readingTimeInMinutes = Math.floor(wordCount / 250) + 1;
+    var readingTimeAsString = readingTimeInMinutes + " min";
+    $('article .reading-time').html(readingTimeAsString);
 
     (function(){
         configurePagination();
