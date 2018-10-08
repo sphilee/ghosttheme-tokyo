0ddb7630676056ffc9e7004c46d0f4382c0afc4f
Todd Birchard <toddbirchard@gmail.com>
2018-10-08 19:45:07 -0400

    restart

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,89 @@
-/*jslint node:true */
-
+// Gulpfile.js running on stratumui,
+// a css framework available on npmjs.com
 'use strict';
 
 var gulp = require('gulp'),
-    del = require('del'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    watch = require('gulp-watch'),
-    livereload = require('gulp-livereload'),
-    minifyCss = require('gulp-minify-css'),
-    autoprefixer = require('gulp-autoprefixer');
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify-es'),
+  rename = require('gulp-rename'),
+  handlebars = require('gulp-handlebars'),
+  declare = require('gulp-declare'),
+  cleanCSS = require('gulp-clean-css'),
+  autoprefixer = require('gulp-autoprefixer'),
+  postcss = require('gulp-postcss'),
+  sourcemaps = require('gulp-sourcemaps'),
+  precss = require('precss'),
+  babel = require('gulp-babel'),
+  resolveDependencies = require('gulp-resolve-dependencies'),
+  livereload = require('gulp-livereload');
 
-gulp.task('styles', function() {
-    return gulp.src('src/sass/*.scss')
-        .pipe(sass({outputStyle: 'expanded', unix_newlines: true, linefeed: "lf"}).on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(minifyCss({
-            keepSpecialComments: 1
+
+var paths = {
+  styles: {
+    src: 'src/sass/*.scss',
+    dest: 'assets/css'
+  },
+  scripts: {
+    src: 'src/js/*.js',
+    dest: 'assets/js'
+  }
+};
+
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write('.', { sourceRoot: '/' }))
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+    }))
+    .pipe(cleanCSS({
+      debug: true
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(postcss([require('precss'), require('autoprefixer')]))
+    .pipe(concat('main.min.css'))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(livereload())
+}
+
+function scripts() {
+  return gulp.src(paths.scripts.src)
+    .pipe(babel({
+          presets: ['@babel/env'],
+          plugins: ['@babel/transform-runtime', '@babel/plugin-syntax-dynamic-import']
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(resolveDependencies({
+            pattern: /\* @requires [\s-]*(.*\.js)/g
         }))
-        .pipe(rename("theme.min.css"))
-        .pipe(gulp.dest('./assets/css/'));
-});
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
 
+function templates() {
+  gulp.src('views/*.hbs')
+    .pipe(handlebars())
+    //.pipe(wrap('Handlebars.template(<%= contents %>)'))
+    .pipe(declare({
+      namespace: 'MyApp.templates',
+      noRedeclare: true, // Avoid duplicate declarations
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('assets/js/'));
+}
 
-gulp.task('scripts', function() {
-    return gulp.src(['src/js/plugin/*.js', 'src/js/base.js'])
-        .pipe(uglify())
-        .pipe(concat('theme.min.js'))
-        .pipe(gulp.dest('assets/js'));
-});
+function watch() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+}
 
+var build = gulp.parallel(styles, scripts, templates, watch);
 
-gulp.task('ghost_config', ['scripts'], function() {
-    return gulp.src(['src/js/config.js', 'assets/js/theme.min.js'])
-        .pipe(concat('theme.min.js'))
-        .pipe(gulp.dest('assets/js'));
-});
-
-
-gulp.task('default', function() {
-    gulp.start('styles', 'scripts', 'ghost_config', 'watch');
-});
-
-
-gulp.task('watch', function() {
-    gulp.watch('src/sass/*.scss', ['styles']);
-    gulp.watch('src/js/*.js', ['scripts', 'ghost_config']);
-    livereload.listen();
-    gulp.watch(['*']).on('change', livereload.changed);
-});
+gulp.task(build);
+gulp.task('default', build);
